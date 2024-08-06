fa49f081d98b6c77845992b523e430374ab2a1ad
Faisal Rahman <qyupaww@gmail.com>
2024-08-06 17:31:27 +0700

    final

--- lib/bg_container.dart
+++ lib/bg_container.dart
@@ -1,1 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:dice_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAligment = Alignment.bottomRight;
+
+class BgContainer extends StatelessWidget {
+  const BgContainer(this.color1, this.color2, {super.key});
+
+  const BgContainer.green({super.key})
+      : color1 = Colors.greenAccent,
+        color2 = Colors.green;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [color1, color2], begin: startAlignment, end: endAligment),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:dice_app/bg_container.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: BgContainer(
+          Color.fromARGB(255, 0, 255, 132),
+          Color.fromARGB(255, 7, 159, 86),
+        ),
+      ),
+    ),
+  );
 }
